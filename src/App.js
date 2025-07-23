fe033db51633fbfcada4b516c91dbfcf2bf297b0
BuvaneshPandiyan <buvaneshpandiyan@gmail.com>
2025-07-23 17:32:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import FAQ from './components/FAQ';
 import CTA from './components/CTA';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import BackToTopButton from './components/WhatsAppChatButton';
+import WhatsAppChatButton from './components/WhatsAppChatButton';
 import About from './components/AboutUs';
 
 function App() {
